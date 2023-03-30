fc54ec18f5718f697c011d0719dfa81c58272e80
Derek Smyth <smyth.derek@gmail.com>
2023-03-29 20:15:14 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for (i = 0; i < word.length / 2; i++) {
+    const j = word.length - 1 - i;
+    if (word[i] !== word[j]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /* 
   Add your pseudocode here
+    We'll be invoking the function with an argument of a lower-case string (single word)
+    To evaluate if it's a palindrome, we need to look at each letter of the string
+    Look at the first letter, then compare it to the last letter
+    Then do the same with the second letter and the second-to-last letter
+    And repeat until you get to the middle of the word
+    Iterate through each letter of the word -- so we need a for loop, with iteration
+    Iterate until the middle of the word -- indicated by being less than half of the string's length
+    If at any point the two compared characters don't match, return false
+    Otherwise return true
 */
 
 /*
